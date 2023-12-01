94d02c1a190300c6f2fbabbf40eb481268af7bc0
oualter <olivier.pretre@gmx.fr>
2023-12-01 17:52:42 +0100

    fix TS

--- src/app/a-propos/page.tsx
+++ src/app/a-propos/page.tsx
@@ -8,7 +8,7 @@ export default async function apropos() {
   return (
     <article className="lg:max-w-[700px] mx-auto my-6">
       <h1 className="text-5xl">{Titre}</h1>
-      {Contenu.map((elt, index) => (
+      {Contenu.map((elt:any, index:number) => (
         <p className="my-4" key={index}>
           {elt.children[0].text}
         </p>
