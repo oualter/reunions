2a1249385e267efee64476976492399160ce34a6
oualter <olivier.pretre@gmx.fr>
2023-12-01 17:55:35 +0100

    fix TS2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css'
-import NavBar from './components/NavBar'
+import NavBar from './components/NavBar.jsx'
 import FooterBar from './components/FooterBar'
 import GetChapitres from './components/GetChapitres'
 
