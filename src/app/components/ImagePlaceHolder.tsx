adddf976bf41782a56ca7ab2e9c8c828f13c6b9e
oualter <olivier.pretre@gmx.fr>
2023-12-05 18:51:49 +0100

    image next

--- src/app/components/ImagePlaceHolder.tsx
+++ src/app/components/ImagePlaceHolder.tsx
@@ -1,12 +1,13 @@
+import Image from 'next/image'
+import reunionMapPic from '../../../public/img/croquis-reunion.png'
 const ImagePlaceHolder = () => {
   return (
     <article className="img-placeholder text-center w-max  mx-auto relative mix-blend-darken">
-      <img
-        src="img/croquis-reunion.png"
+      <Image
+        src={reunionMapPic}
         width="1000"
         height="1000"
-        alt=""
-        // className="w-[120rem] -top-[111px] -mb-[111px] relative "
+        alt="Place de La Réunion"
         className="relative min-h-screen"
       />
     </article>
