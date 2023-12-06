5fecfe75505163ea9cef51a0dcbedd3fe3adc420
oualter <olivier.pretre@gmx.fr>
2023-12-06 13:54:02 +0100

    fix error

--- src/app/le-livre/page.jsx
+++ src/app/le-livre/page.jsx
@@ -11,13 +11,13 @@ export default async function leLivre() {
     return (
       <article className="lg:max-w-[700px] mx-auto my-6">
         <h1 className="text-5xl">{Titre}</h1>
-        {Contenu.map((elt:any, index:number) => (
+        {Contenu.map((elt, index) => (
           <p className="my-4" key={index}>
             {elt.children[0].text}
           </p>
         ))}
         <br />
-        {contenu_2.map((elt:any, index:number) => (
+        {contenu_2.map((elt,index) => (
           <p className="my-4" key={index}>
             {elt.children[0].text}
           </p>
